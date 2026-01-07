de8d6a28f03e2b00176ace06a783a5d30e33abd1
Surjay_Soni <surjay.qualwebs@gmail.com>
2026-01-07 15:38:29 +0530

    Updating Changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-document.getElementById("actionBtn").onclick = () => {
-  alert("Button clicked!");
-};
+const btn = document.getElementById("actionBtn");
+const status = document.getElementById("status");
 
+btn.addEventListener("click", () => {
+  btn.disabled = true;
+  btn.innerText = "Processing...";
+  status.innerText = "Please wait...";
+
+  setTimeout(() => {
+    btn.disabled = false;
+    btn.innerText = "Submit";
+    status.innerText = "Done!";
+  }, 2000);
+});
